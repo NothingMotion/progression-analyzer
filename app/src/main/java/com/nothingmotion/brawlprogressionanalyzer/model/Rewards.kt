cccf9a90fc92ad053cc1dc9e51716c85f2ccff7b
itsma8di <mrmmrahmati@gmail.com>
2025-04-04 19:08:00 +0330

    added Rewards data models

---
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/model/Rewards.kt
@@ -0,0 +1,34 @@
+package com.nothingmotion.brawlprogressionanalyzer.model
+
+
+sealed class Rewards (
+    open val id : Int,
+    open val name: String,
+)
+
+sealed class PassRewards (
+    override val id: Int,
+    override val name: String = "PassReward",
+    open val resources: List<Resource>
+) : Rewards(id,name)
+data class BrawlPassRewards(
+    override val id: Int,
+    override val name: String,
+    override val resources: List<Resource>
+) : PassRewards(id,name,resources)
+data class BrawlPassPlusRewards(
+    override val id: Int,
+    override val name: String,
+    override val resources: List<Resource>
+) : PassRewards(id,name,resources)
+data class StarrDropReward(
+    val resource: Resource,
+    val chance: Float
+)
+data class StarrDropRewards(
+    override val id: Int,
+    override val name: String,
+    val rarity: RarityData,
+    val chanceToDrop: Float,
+    val rewards: List<StarrDropReward>
+) : Rewards(id,name)
