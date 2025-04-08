c5c0a03186329f3cd5dacaa48e62e91d464ed773
itsma8di <mrmmrahmati@gmail.com>
2025-04-08 21:39:44 +0330

    added FakeStarrDropTableRepository to generate fake rewards based on starrdrop

---
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/repository/FakeStarrDropTableRepository.kt
@@ -0,0 +1,122 @@
+package com.nothingmotion.brawlprogressionanalyzer.data.repository
+
+import com.nothingmotion.brawlprogressionanalyzer.model.Bling
+import com.nothingmotion.brawlprogressionanalyzer.model.BrawlerResource
+import com.nothingmotion.brawlprogressionanalyzer.model.Coin
+import com.nothingmotion.brawlprogressionanalyzer.model.Credit
+import com.nothingmotion.brawlprogressionanalyzer.model.GadgetResource
+import com.nothingmotion.brawlprogressionanalyzer.model.HyperCharge
+import com.nothingmotion.brawlprogressionanalyzer.model.Pin
+import com.nothingmotion.brawlprogressionanalyzer.model.PowerPoint
+import com.nothingmotion.brawlprogressionanalyzer.model.ProfileIcon
+import com.nothingmotion.brawlprogressionanalyzer.model.RarityData
+import com.nothingmotion.brawlprogressionanalyzer.model.Skin
+import com.nothingmotion.brawlprogressionanalyzer.model.Spray
+import com.nothingmotion.brawlprogressionanalyzer.model.StarPowerResource
+import com.nothingmotion.brawlprogressionanalyzer.model.StarrDropReward
+import com.nothingmotion.brawlprogressionanalyzer.model.StarrDropRewards
+import com.nothingmotion.brawlprogressionanalyzer.model.XPDoubler
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class FakeStarrDropTableRepository @Inject constructor(){
+    private val _starrDropTable = MutableStateFlow<List<StarrDropRewards>>(emptyList())
+    val starrDropTable get() = _starrDropTable.asStateFlow()
+    
+    init {
+        _starrDropTable.value = generateStarrDropTable()
+    }
+    
+    private fun generateStarrDropTable(): List<StarrDropRewards> {
+        return listOf(
+            // StarrDrop - Rare
+            StarrDropRewards(
+                id = 1,
+                name = "StarrDrop",
+                rarity = RarityData.RARE,
+                chanceToDrop = 0.5f,
+                rewards = listOf(
+                    StarrDropReward(Coin(amount = 50), 0.419f),
+                    StarrDropReward(PowerPoint(amount = 25), 0.326f),
+                    StarrDropReward(XPDoubler(amount = 1), 0.209f),
+                    StarrDropReward(Bling(amount = 20), 0.023f),
+                    StarrDropReward(Credit(amount = 10), 0.023f)
+                )
+            ),
+            
+            // StarrDrop - SuperRare
+            StarrDropRewards(
+                id = 2,
+                name = "StarrDrop",
+                rarity = RarityData.SUPER_RARE,
+                chanceToDrop = 0.28f,
+                rewards = listOf(
+                    StarrDropReward(Coin(amount = 100), 0.4238f),
+                    StarrDropReward(PowerPoint(amount = 50), 0.3311f),
+                    StarrDropReward(XPDoubler(amount = 200), 0.1325f),
+                    StarrDropReward(Bling(amount = 50), 0.0331f),
+                    StarrDropReward(Credit(amount = 30), 0.0331f),
+                    StarrDropReward(Pin(amount = 1), 0.0331f),
+                    StarrDropReward(Spray(amount = 1), 0.0331f)
+                )
+            ),
+            
+            // StarrDrop - Epic
+            StarrDropRewards(
+                id = 3,
+                name = "StarrDrop",
+                rarity = RarityData.EPIC,
+                chanceToDrop = 0.15f,
+                rewards = listOf(
+                    StarrDropReward(Coin(amount = 200), 0.2105f),
+                    StarrDropReward(PowerPoint(amount = 100), 0.2105f),
+                    StarrDropReward(Pin(amount = 1), 0.1579f),
+                    StarrDropReward(Spray(amount = 1), 0.1579f),
+                    StarrDropReward(XPDoubler(amount = 500), 0.1053f),
+                    StarrDropReward(BrawlerResource(amount = 1, rarity = RarityData.RARE), 0.0526f)
+                )
+            ),
+            
+            // StarrDrop - Mythic
+            StarrDropRewards(
+                id = 4,
+                name = "StarrDrop",
+                rarity = RarityData.MYTHIC,
+                chanceToDrop = 0.05f,
+                rewards = listOf(
+                    StarrDropReward(PowerPoint(amount = 200), 0.1899f),
+                    StarrDropReward(GadgetResource(amount = 1), 0.1582f),
+                    StarrDropReward(Skin(amount = 1, rarity = RarityData.RARE), 0.1582f),
+                    StarrDropReward(Coin(amount = 500), 0.0949f),
+                    StarrDropReward(BrawlerResource(amount = 1, rarity = RarityData.SUPER_RARE), 0.0949f),
+                    StarrDropReward(BrawlerResource(amount = 1, rarity = RarityData.EPIC), 0.0633f),
+                    StarrDropReward(ProfileIcon(amount = 1), 0.0633f),
+                    StarrDropReward(Pin(amount = 1), 0.0633f),
+                    StarrDropReward(Spray(amount = 1), 0.0633f),
+                    StarrDropReward(Pin(amount = 1), 0.0316f),
+                    StarrDropReward(BrawlerResource(amount = 1, rarity = RarityData.MYTHIC), 0.0190f)
+                )
+            ),
+            
+            // StarrDrop - Legendary
+            StarrDropRewards(
+                id = 5,
+                name = "StarrDrop",
+                rarity = RarityData.LEGENDARY,
+                chanceToDrop = 0.02f,
+                rewards = listOf(
+                    StarrDropReward(Skin(amount = 1, rarity = RarityData.SUPER_RARE), 0.3587f),
+                    StarrDropReward(StarPowerResource(amount = 1), 0.2717f),
+                    StarrDropReward(HyperCharge(amount = 1), 0.1630f),
+                    StarrDropReward(BrawlerResource(amount = 1, rarity = RarityData.EPIC), 0.1087f),
+                    StarrDropReward(BrawlerResource(amount = 1, rarity = RarityData.MYTHIC), 0.0543f),
+                    StarrDropReward(Skin(amount = 1, rarity = RarityData.EPIC), 0.0217f),
+                    StarrDropReward(BrawlerResource(amount = 1, rarity = RarityData.LEGENDARY), 0.0217f)
+                )
+            )
+        )
+    }
+}
