d0dffd95fa70469c04f5c7405669d7ce90bae880
itsma8di <mrmmrahmati@gmail.com>
2025-04-08 21:30:40 +0330

    added StarrDropUtils.kt

---
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/util/StarrDropUtils.kt
@@ -0,0 +1,28 @@
+package com.nothingmotion.brawlprogressionanalyzer.util
+
+import com.nothingmotion.brawlprogressionanalyzer.data.repository.FakeStarrDropTableRepository
+import com.nothingmotion.brawlprogressionanalyzer.model.StarrDropReward
+import com.nothingmotion.brawlprogressionanalyzer.model.StarrDropRewards
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class StarrDropUtils {
+
+    @Inject
+    lateinit var starrDropTableRepository: FakeStarrDropTableRepository
+    fun predictStarrDrops(amountToOpen: Int) : List<StarrDropRewards>{
+            val chances : MutableList<StarrDropRewards> = mutableListOf()
+            val tables = starrDropTableRepository.starrDropTable.value
+
+            for (table : StarrDropRewards in tables){
+                table.chanceToDrop *= amountToOpen
+                for(reward : StarrDropReward in table.rewards){
+                    reward.chance *= amountToOpen
+                }
+                chances.add(table);
+            }
+            return chances
+
+        }
+}
