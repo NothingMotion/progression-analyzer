3f60f7200e61d5a9c057702e8fa3694098f43dcb
itsma8di <mrmmrahmati@gmail.com>
2025-04-18 18:20:56 +0330

    added APIUpdate model to APINotMot

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/model/APINotMot.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/model/APINotMot.kt
@@ -1,6 +1,14 @@
 package com.nothingmotion.brawlprogressionanalyzer.data.remote.model
 
 
+data class APIUpdate(
+    val name: String,
+    val version: Long,
+    val versionString: String,
+    val forceUpdate: Boolean,
+    val updateDescription: String,
+    val url: String,
+)
 data class APIToken(
     val message: String?,
     val token: String,
