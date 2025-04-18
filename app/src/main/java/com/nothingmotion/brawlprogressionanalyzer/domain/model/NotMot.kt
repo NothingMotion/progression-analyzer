232dafba96a70a3e96fa6f1d7e86a2ca15eb25db
itsma8di <mrmmrahmati@gmail.com>
2025-04-18 20:53:13 +0330

    update: updating track model

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/model/NotMot.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/model/NotMot.kt
@@ -1,7 +1,11 @@
 package com.nothingmotion.brawlprogressionanalyzer.domain.model
 
 import com.google.gson.GsonBuilder
+import java.text.SimpleDateFormat
+import java.time.Instant
 import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 import java.util.UUID
 
 
@@ -11,11 +15,16 @@ data class Track(
     val joinedSince: Date = Date(),
     val version: Long? = 0,
     val versionString: String? = "",
-
+    val date: Date = Date()
 )
 fun Track.toJson() : String{
     return GsonBuilder().create().toJson(this)
 }
 fun String.toTrack() : Track? {
     return GsonBuilder().create().fromJson(this,Track::class.java)
+}
+fun nowString() : String{
+    val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",Locale.US)
+    sdf.timeZone = TimeZone.getTimeZone("UTCC")
+    return sdf.format(Date())
 }
