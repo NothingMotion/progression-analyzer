adfca7ff6df2cbd3cd59b6bea6fa8fddbb1b7846
itsma8di <mrmmrahmati@gmail.com>
2025-04-18 14:29:37 +0330

    changed return types on BrawlerRepository

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/repository/BrawlerRepository.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/repository/BrawlerRepository.kt
@@ -1,8 +1,11 @@
 package com.nothingmotion.brawlprogressionanalyzer.domain.repository
 
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.BrawlerData
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.DataError
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.Result
+import kotlinx.coroutines.flow.Flow
 
 interface BrawlerRepository {
-    suspend fun getBrawler(id: Long): BrawlerData?
-    suspend fun getBrawlers(): List<BrawlerData>
+    suspend fun getBrawler(id: Long): Result<BrawlerData,DataError.NetworkError>
+    suspend fun getBrawlers(): Flow<Result<List<BrawlerData>,DataError.NetworkError>>
 }
