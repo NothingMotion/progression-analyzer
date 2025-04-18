770818ec8f6a86c6eb8af6183e3da951ccca6303
itsma8di <mrmmrahmati@gmail.com>
2025-04-18 16:12:20 +0330

    added PassRepository and PassRewardsRepositoryImpl

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/repository/PassRepository.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/repository/PassRepository.kt
@@ -1,9 +1,11 @@
 package com.nothingmotion.brawlprogressionanalyzer.domain.repository
 
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.DataError
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.PassRewards
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.Result
 
 interface PassRepository {
-    suspend fun getPassFreeTable(): PassRewards?
-    suspend fun getPassPremiumTable(): PassRewards?
-    suspend fun getPassPlusTable(): PassRewards?
+    suspend fun getPassFreeTable(token: String): Result<PassRewards,DataError.NetworkError>
+    suspend fun getPassPremiumTable(token : String): Result<PassRewards,DataError.NetworkError>
+    suspend fun getPassPlusTable(token : String): Result<PassRewards,DataError.NetworkError>
 }
