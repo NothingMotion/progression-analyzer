633a9a82bd397370163fe04c60cd6cba1292a730
itsma8di <mrmmrahmati@gmail.com>
2025-04-18 18:04:20 +0330

    updated retrofit client timeouts

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/di/RetrofitBuilderModule.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/di/RetrofitBuilderModule.kt
@@ -7,8 +7,11 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.time.Duration
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -20,9 +23,17 @@ object RetrofitBuilderModule {
     @Singleton
     @Provides
     fun provideRetrofit(): Retrofit {
+        val CONNECTION_TIMEOUT = 5000L;
+        val client = OkHttpClient.Builder()
+            .connectTimeout(CONNECTION_TIMEOUT,TimeUnit.MILLISECONDS)
+            .readTimeout(CONNECTION_TIMEOUT,TimeUnit.MILLISECONDS)
+            .writeTimeout(CONNECTION_TIMEOUT,TimeUnit.MILLISECONDS)
+            .callTimeout(CONNECTION_TIMEOUT,TimeUnit.MILLISECONDS)
+            .build()
         return Retrofit.Builder()
             .baseUrl(BuildConfig.PROGRESSION_ANALYZER_API)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
     }
 
