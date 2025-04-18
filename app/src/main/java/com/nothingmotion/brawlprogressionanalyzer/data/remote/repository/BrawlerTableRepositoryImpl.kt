0d2270fe118c41191d2e4bae40dadc929dc318fa
itsma8di <mrmmrahmati@gmail.com>
2025-04-18 14:55:24 +0330

    added BrawlerTableRepository and BrawlerTableRepositoryImpl

---
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/repository/BrawlerTableRepositoryImpl.kt
@@ -0,0 +1,41 @@
+package com.nothingmotion.brawlprogressionanalyzer.data.remote.repository
+
+import com.nothingmotion.brawlprogressionanalyzer.data.remote.ProgressionAnalyzerAPI
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.BrawlerTable
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.DataError
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.Result
+import com.nothingmotion.brawlprogressionanalyzer.domain.repository.BrawlerTableRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class BrawlerTableRepositoryImpl : BrawlerTableRepository {
+    @Inject lateinit var api: ProgressionAnalyzerAPI
+    override suspend fun getBrawlerTable(token: String): Flow<Result<List<BrawlerTable>,DataError.NetworkError>> {
+        return flow {
+            try {
+                emit(Result.Success(api.getBrawlerTable(token)))
+            }
+            catch (e: IOException) {
+                emit(Result.Error(DataError.NetworkError.NO_INTERNET_CONNECTION))
+            } catch (e: HttpException) {
+                emit(
+                    when (e.code()) {
+                        400 -> Result.Error(DataError.NetworkError.NETWORK_ERROR)
+                        401 -> Result.Error(DataError.NetworkError.UNAUTHORIZED)
+                        403 -> Result.Error(DataError.NetworkError.FORBIDDEN)
+                        429 -> Result.Error(DataError.NetworkError.TOO_MANY_REQUESTS)
+                        500 -> Result.Error(DataError.NetworkError.SERVER_ERROR)
+                        else -> Result.Error(DataError.NetworkError.UNKNOWN)
+                    }
+                )
+            }
+            catch (e: Exception) {
+                emit(Result.Error(DataError.NetworkError.UNKNOWN))
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/repository/BrawlerTableRepository.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/repository/BrawlerTableRepository.kt
@@ -1,7 +1,10 @@
 package com.nothingmotion.brawlprogressionanalyzer.domain.repository
 
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.BrawlerTable
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.DataError
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.Result
+import kotlinx.coroutines.flow.Flow
 
 interface BrawlerTableRepository {
-    suspend fun getBrawlerTable() : BrawlerTable?
+    suspend fun getBrawlerTable(token: String) : Flow<Result<List<BrawlerTable>, DataError.NetworkError>>
 }
