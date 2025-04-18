79f3f9ce9951515f0e41691f373964a21e21df7d
itsma8di <mrmmrahmati@gmail.com>
2025-04-18 14:04:38 +0330

    added getAllAccounts and getAccountHistory to AccountRepository and Implemented on AccountRepositoryImpl

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/repository/AccountRepositoryImpl.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/repository/AccountRepositoryImpl.kt
@@ -1,27 +1,35 @@
-package com.nothingmotion.brawlprogressionanalyzer.data.repository
+    package com.nothingmotion.brawlprogressionanalyzer.data.repository
 
 import com.nothingmotion.brawlprogressionanalyzer.data.ProgressionAnalyzerAPI
 import com.nothingmotion.brawlprogressionanalyzer.data.mappers.toAccount
+import com.nothingmotion.brawlprogressionanalyzer.data.model.History
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Account
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.DataError
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Result
 import com.nothingmotion.brawlprogressionanalyzer.domain.repository.AccountRepository
 import com.nothingmotion.brawlprogressionanalyzer.util.TokenManager
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
 
-class AccountRepositoryImpl constructor(@Inject val api: ProgressionAnalyzerAPI,@Inject val tokenManager: TokenManager): AccountRepository {
-    override suspend fun getAccount(tag: String):Result<Account, DataError.NetworkError> {
+class AccountRepositoryImpl @Inject constructor(
+
+) : AccountRepository {
+
+    @Inject
+    lateinit var api: ProgressionAnalyzerAPI
+    @Inject
+    lateinit var tokenManager: TokenManager
+    override suspend fun getAccount(tag: String): Result<Account, DataError.NetworkError> {
         try {
             val token = tokenManager.getAccessToken("")
-            return Result.Success(api.getAccount(tag,token).toAccount())
-        }
-        catch(e: IOException){
+            return Result.Success(api.getAccount(tag, token).toAccount())
+        } catch (e: IOException) {
             return Result.Error(DataError.NetworkError.NO_INTERNET_CONNECTION)
-        }
-        catch(e: HttpException){
-            return when (e.code()){
+        } catch (e: HttpException) {
+            return when (e.code()) {
                 400 -> Result.Error(DataError.NetworkError.NETWORK_ERROR)
                 401 -> Result.Error(DataError.NetworkError.UNAUTHORIZED)
                 403 -> Result.Error(DataError.NetworkError.FORBIDDEN)
@@ -29,13 +37,47 @@ class AccountRepositoryImpl constructor(@Inject val api: ProgressionAnalyzerAPI,
                 500 -> Result.Error(DataError.NetworkError.SERVER_ERROR)
                 else -> Result.Error(DataError.NetworkError.UNKNOWN)
             }
-        }
-        catch(e : Exception){
+        } catch (e: Exception) {
             return Result.Error(DataError.NetworkError.UNKNOWN)
         }
     }
 
-    override suspend fun getAllAccounts(): List<Account> {
-        TODO("Not yet implemented")
+    override suspend fun getAccountHistory(
+        tag: String,
+        limit: Int?,
+        offset: Int?
+    ): Flow<Result<List<History>, DataError.NetworkError>> {
+        return flow {
+            try {
+                val token = tokenManager.getAccessToken("")
+                emit(Result.Success(api.getAccountHistory(tag, limit, offset, token)))
+            } catch (e: IOException) {
+                emit(Result.Error(DataError.NetworkError.NO_INTERNET_CONNECTION))
+            } catch (e: HttpException) {
+                emit(
+                    when (e.code()) {
+                        400 -> Result.Error(DataError.NetworkError.NETWORK_ERROR)
+                        401 -> Result.Error(DataError.NetworkError.UNAUTHORIZED)
+                        403 -> Result.Error(DataError.NetworkError.FORBIDDEN)
+                        429 -> Result.Error(DataError.NetworkError.TOO_MANY_REQUESTS)
+                        500 -> Result.Error(DataError.NetworkError.SERVER_ERROR)
+                        else -> Result.Error(DataError.NetworkError.UNKNOWN)
+                    }
+                )
+            } catch (e: Exception) {
+                emit(Result.Error(DataError.NetworkError.UNKNOWN))
+            }
+        }
+    }
+
+    override suspend fun getAllAccounts(): Flow<Result<List<Account>,DataError.NetworkError>> {
+        return flow {
+            try {
+                emit(Result.Success(api.getAccounts().map { it.toAccount() }))
+            }
+            catch(e: Exception){
+                emit(Result.Error(DataError.NetworkError.UNKNOWN))
+            }
+        }
     }
 }
--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/repository/AccountRepository.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/repository/AccountRepository.kt
@@ -1,10 +1,14 @@
 package com.nothingmotion.brawlprogressionanalyzer.domain.repository
 
+import com.nothingmotion.brawlprogressionanalyzer.data.model.History
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Account
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.DataError
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Result
+import kotlinx.coroutines.flow.Flow
 
 interface AccountRepository {
     suspend fun getAccount(tag: String) : Result<Account,DataError.NetworkError>
-    suspend fun getAllAccounts(): List<Account>
+    suspend fun getAllAccounts(): Flow<Result<List<Account>,DataError.NetworkError>>
+
+    suspend fun getAccountHistory(tag: String,limit : Int?, offset: Int?) : Flow<Result<List<History>,DataError.NetworkError>>
 }
