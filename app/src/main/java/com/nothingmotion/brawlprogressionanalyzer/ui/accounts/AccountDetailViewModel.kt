1ac24e79d2dd165e2cf9a17901299133f9d1943f
itsma8di <mrmmrahmati@gmail.com>
2025-04-19 14:37:55 +0330

    fix: commented out old way of retriving account detail on AccountDetailViewModel

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/ui/accounts/AccountDetailViewModel.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/ui/accounts/AccountDetailViewModel.kt
@@ -2,18 +2,25 @@ package com.nothingmotion.brawlprogressionanalyzer.ui.accounts
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.nothingmotion.brawlprogressionanalyzer.data.PreferencesManager
 import com.nothingmotion.brawlprogressionanalyzer.data.remote.repository.fake.FakeAccountRepository
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Account
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.Result
+import com.nothingmotion.brawlprogressionanalyzer.domain.repository.AccountRepository
+import com.nothingmotion.brawlprogressionanalyzer.util.TokenManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import java.util.Date
 import javax.inject.Inject
 
 @HiltViewModel
 class AccountDetailViewModel @Inject constructor(
-    private val accountRepository: FakeAccountRepository
+    private val accountRepository: AccountRepository,
+    private val preferencesManager: PreferencesManager,
+    private val tokenManager: TokenManager
 ) : ViewModel() {
     
     private val _account = MutableStateFlow<Account?>(null)
@@ -24,8 +31,18 @@ class AccountDetailViewModel @Inject constructor(
      */
     fun getAccount(accountId: String) {
         viewModelScope.launch {
-            val result = accountRepository.getAccount(accountId)
-            _account.value = result
+            preferencesManager.track?.let {
+                val token = tokenManager.getAccessToken(it.uuid.toString())
+//                token?.let {
+//                    val result = accountRepository.getAccount(accountId.replace("#",""),token)
+//                    when(result){
+//                        is Result.Error -> Timber.tag("AccountDetailViewModel").e(result.error.name)
+//                        is Result.Loading -> TODO()
+//                        is Result.Success ->_account.value = result.data
+//                    }
+//
+//                }
+            }
         }
     }
     
@@ -34,7 +51,7 @@ class AccountDetailViewModel @Inject constructor(
      */
     fun updateAccount(account: Account) {
         viewModelScope.launch {
-            accountRepository.updateAccount(account)
+//            accountRepository.updateAccount(account)
             _account.value = account
         }
     }
@@ -44,15 +61,15 @@ class AccountDetailViewModel @Inject constructor(
      */
     fun updateAccountTag(accountId: String, newTag: String) {
         viewModelScope.launch {
-            val account = accountRepository.getAccount(accountId)
+//            val account = accountRepository.getAccount(accountId)
             account?.let {
-                val updatedPlayer = it.account.copy(tag = newTag)
-                val updatedAccount = it.copy(
-                    account = updatedPlayer,
-                    updatedAt = Date()
-                )
-                accountRepository.updateAccount(updatedAccount)
-                _account.value = updatedAccount
+//                val updatedPlayer = it.account.copy(tag = newTag)
+//                val updatedAccount = it.copy(
+//                    account = updatedPlayer,
+//                    updatedAt = Date()
+//                )
+//                accountRepository.updateAccount(updatedAccount)
+//                _account.value = updatedAccount
             }
         }
     }
@@ -63,7 +80,7 @@ class AccountDetailViewModel @Inject constructor(
     fun deleteAccount() {
         viewModelScope.launch {
             _account.value?.let { account ->
-                accountRepository.deleteAccount(account.account.id)
+//                accountRepository.deleteAccount(account.account.id)
                 _account.value = null
             }
         }
