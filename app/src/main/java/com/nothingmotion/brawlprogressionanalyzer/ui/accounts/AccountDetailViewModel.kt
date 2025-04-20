290e6c6a23430fe4b2f68ba9b1778a4c49039ab8
itsma8di <mrmmrahmati@gmail.com>
2025-04-20 09:48:38 +0330

    update: add AccountDetailState, findAccountJob update: getAccount method to use findAccountJob and add state handling for account fetching update: add stop method for findAccountJob fix: comment out old repository methods for fetching account

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/ui/accounts/AccountDetailViewModel.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/ui/accounts/AccountDetailViewModel.kt
@@ -9,50 +9,94 @@ import com.nothingmotion.brawlprogressionanalyzer.domain.model.Result
 import com.nothingmotion.brawlprogressionanalyzer.domain.repository.AccountRepository
 import com.nothingmotion.brawlprogressionanalyzer.util.TokenManager
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import timber.log.Timber
-import java.util.Date
 import javax.inject.Inject
 
 @HiltViewModel
 class AccountDetailViewModel @Inject constructor(
     private val accountRepository: AccountRepository,
+    private val fakeAccountRepository: FakeAccountRepository,
     private val preferencesManager: PreferencesManager,
     private val tokenManager: TokenManager
 ) : ViewModel() {
     
-    private val _account = MutableStateFlow<Account?>(null)
-    val account: StateFlow<Account?> = _account
-    
+    private val _state = MutableStateFlow<AccountDetailState>(AccountDetailState(loading=true))
+    val state: StateFlow<AccountDetailState> = _state
+
+    private var findAccountJob : Job? = null
+    private var getTokenJob : Job? = null
+
+
+
+
+
+
+
+
+    fun getFakeAccount(accountId: String){
+        viewModelScope.launch {
+            delay(5000)
+            _state.update {  it.copy(account= fakeAccountRepository.getAccount(accountId), loading = false,error= null)}
+        }
+    }
     /**
      * Load the account details from the repository
      */
     fun getAccount(accountId: String) {
-        viewModelScope.launch {
+        findAccountJob = viewModelScope.launch {
             preferencesManager.track?.let {
-                val token = tokenManager.getAccessToken(it.uuid.toString())
-//                token?.let {
-//                    val result = accountRepository.getAccount(accountId.replace("#",""),token)
-//                    when(result){
-//                        is Result.Error -> Timber.tag("AccountDetailViewModel").e(result.error.name)
-//                        is Result.Loading -> TODO()
-//                        is Result.Success ->_account.value = result.data
-//                    }
-//
-//                }
+                _state.update { it.copy(loading=true,error=null) }
+                tokenManager.getAccessToken(it.uuid.toString())
+                tokenManager.accessTokenState.collectLatest {state->
+                    if(state.loading){
+                        _state.update { it.copy(loading = true,error= null) }
+                    }
+                    else if(state.error != null){
+                        _state.update { it.copy(loading = false,error=state.error) }
+                    }
+                    else {
+                        state.success?.let {
+                            val result = accountRepository.getAccount(accountId.replace("#",""),it)
+                            when(result){
+                                is Result.Error -> {
+                                    Timber.tag("AccountDetailViewModel").e(result.error.name)
+                                    _state.update { it.copy(error=result.error.name,loading=false) }
+                                }
+                                is Result.Loading -> {
+                                    _state.update { it.copy(loading = true,error=null) }
+                                }
+                                is Result.Success ->_state.update {  it.copy(account=result.data,error=null,loading=false)}
+                            }
+
+                        }
+                    }
+                }
             }
         }
     }
     
+
+
+    fun stop(){
+        findAccountJob?.cancel()
+        Timber.tag("AccountDetailViewModel").d("Finding Account job was cancelled.")
+    }
+
+
     /**
      * Update an account
      */
     fun updateAccount(account: Account) {
         viewModelScope.launch {
 //            accountRepository.updateAccount(account)
-            _account.value = account
+//            _state.value = account
         }
     }
     
@@ -62,7 +106,7 @@ class AccountDetailViewModel @Inject constructor(
     fun updateAccountTag(accountId: String, newTag: String) {
         viewModelScope.launch {
 //            val account = accountRepository.getAccount(accountId)
-            account?.let {
+            state?.let {
 //                val updatedPlayer = it.account.copy(tag = newTag)
 //                val updatedAccount = it.copy(
 //                    account = updatedPlayer,
@@ -79,10 +123,15 @@ class AccountDetailViewModel @Inject constructor(
      */
     fun deleteAccount() {
         viewModelScope.launch {
-            _account.value?.let { account ->
+            _state.value?.let { account ->
 //                accountRepository.deleteAccount(account.account.id)
-                _account.value = null
+//                _state.value = null
             }
         }
     }
+    data class AccountDetailState(
+        val account: Account? = null,
+        val error: String? = null,
+        val loading: Boolean = false
+    )
 } 
