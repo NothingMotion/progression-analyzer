29a8b17881f1ea2d9aae3ecdae1d83dc3f0d0a46
itsma8di <mrmmrahmati@gmail.com>
2025-04-26 18:53:36 +0330

    feat: add glide module

---
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/di/AppGlideModule.kt
@@ -0,0 +1,6 @@
+package com.nothingmotion.brawlprogressionanalyzer.di
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+@GlideModule
+class AppGlideModule : AppGlideModule()
