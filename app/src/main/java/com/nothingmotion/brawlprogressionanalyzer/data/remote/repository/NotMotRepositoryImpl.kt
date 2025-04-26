1819e55c3e79dce66b7ebf10dd85ab34e3fb44d7
itsma8di <mrmmrahmati@gmail.com>
2025-04-26 18:39:44 +0330

    feat: add reportCrash to NotMotRepository and implement it

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/repository/NotMotRepositoryImpl.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/repository/NotMotRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.nothingmotion.brawlprogressionanalyzer.data.remote.repository
 
+import android.util.Log
+import com.nothingmotion.brawlprogressionanalyzer.crashlytics.common.CrashLytics
 import com.nothingmotion.brawlprogressionanalyzer.data.remote.ProgressionAnalyzerAPI
 import com.nothingmotion.brawlprogressionanalyzer.data.remote.model.APIUpdate
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.DataError
@@ -7,6 +9,7 @@ import com.nothingmotion.brawlprogressionanalyzer.domain.model.Result
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Track
 import com.nothingmotion.brawlprogressionanalyzer.domain.repository.NotMotRepository
 import retrofit2.HttpException
+import timber.log.Timber
 import java.io.IOException
 import javax.inject.Inject
 
@@ -57,6 +60,36 @@ class NotMotRepositoryImpl @Inject constructor() : NotMotRepository {
         }
         catch(e: Exception){
             return Result.Error(DataError.NetworkError.UNKNOWN)
+        }
+    }
+
+    override suspend fun reportCrash(
+        token: String,
+        data: CrashLytics.CrashReport
+    ): Result<CrashLytics.CrashReport, DataError.NetworkError> {
+        return try{
+            Result.Success(api.newCrash("Bearer $token",data))
+        }
+        catch(e: IOException){
+            return Result.Error(DataError.NetworkError.NO_INTERNET_CONNECTION)
+        }
+        catch(e: HttpException){
+            return when (e.code()){
+                400 -> Result.Error(DataError.NetworkError.NETWORK_ERROR)
+                401 -> Result.Error(DataError.NetworkError.UNAUTHORIZED)
+                403 -> Result.Error(DataError.NetworkError.FORBIDDEN)
+                429 -> Result.Error(DataError.NetworkError.TOO_MANY_REQUESTS)
+                500 -> Result.Error(DataError.NetworkError.SERVER_ERROR)
+                else -> {
+                    Timber.tag("NotMotRepositoryImpl").e(e);
+                    Result.Error(DataError.NetworkError.UNKNOWN)
+                }
+            }
+        }
+        catch (e:Exception){
+
+            Log.e("NotMotRepositoryImpl",e.stackTraceToString())
+            Result.Error(DataError.NetworkError.UNKNOWN)
         }
     }
 }
--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/repository/NotMotRepository.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/repository/NotMotRepository.kt
@@ -1,5 +1,6 @@
 package com.nothingmotion.brawlprogressionanalyzer.domain.repository
 
+import com.nothingmotion.brawlprogressionanalyzer.crashlytics.common.CrashLytics
 import com.nothingmotion.brawlprogressionanalyzer.data.remote.model.APIUpdate
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.DataError
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Result
@@ -10,4 +11,6 @@ interface NotMotRepository {
     suspend fun getLatestUpdate(token: String) : Result<APIUpdate,DataError.NetworkError>
 
     suspend fun trackUser(token:String, data: Track): Result<Track,DataError.NetworkError>
+
+    suspend fun reportCrash(token: String, data: CrashLytics.CrashReport): Result<CrashLytics.CrashReport,DataError.NetworkError>
 }
