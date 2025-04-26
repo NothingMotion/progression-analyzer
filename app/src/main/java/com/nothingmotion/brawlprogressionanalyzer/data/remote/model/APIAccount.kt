1e0f171f7036b462dee1ed49c60cdc9979bb1fd6
itsma8di <mrmmrahmati@gmail.com>
2025-04-26 18:56:47 +0330

    fix: change some data types

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/model/APIAccount.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/model/APIAccount.kt
@@ -1,40 +1,23 @@
 package com.nothingmotion.brawlprogressionanalyzer.data.remote.model
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Brawler
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.Icon
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.Player
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Progress
 import java.util.Date
-import java.util.UUID
 
 /**
  * Data model representing a player account in Brawl Stars
  */
-
-data class Icon(
-    val id: Long,
-//    val url: String
+data class APIHistory(
+    val accountId: String,
+    val name: String,
+    val trophies: Int,
+    val highestTrophies: Int,
+    val level: Int,
+    val icon: Icon? = null,
+    val brawlers: List<Brawler> = emptyList(),
+    val createdAt: Date = Date()
 )
-
-open class Player(
-//    val id: String = UUID.randomUUID().toString(),
-    open val name: String,
-    open val tag: String,
-    open val trophies: Int,
-    open val highestTrophies: Int,
-    open val level: Int,
-    open val icon: Icon? = null,
-    open val brawlers: List<Brawler> = emptyList(),
-    open val createdAt: Date = Date()
-)
-data class History(
-//    id: String = UUID.randomUUID().toString(),
-    override val name: String,
-    override val tag: String,
-    override val trophies: Int,
-    override val highestTrophies: Int,
-    override val level: Int,
-    override val icon: Icon? = null,
-    override val brawlers: List<Brawler> = emptyList(),
-    override val createdAt: Date = Date()
-): Player(name, tag, trophies, highestTrophies, level, icon, brawlers, createdAt)
 data class APIAccount(
     val account: Player,
 //    val history: List<Player>? = null,
@@ -43,4 +26,13 @@ data class APIAccount(
     val futureProgresses: List<Progress>? = null,
     val createdAt: Date = Date(),
     val updatedAt: Date = Date()
+)
+
+data class APIAccountsResult(
+    val accounts: List<APIAccount>
+)
+
+data class APIHistoryResult(
+
+    val history: List<APIHistory>
 )
