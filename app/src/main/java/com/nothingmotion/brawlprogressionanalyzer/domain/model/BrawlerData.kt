65caa0de6f76762d75be0aa34dac45c311fbb525
itsma8di <mrmmrahmati@gmail.com>
2025-04-26 18:44:01 +0330

    fix: add inheritance for brawler ability on BrawlerData

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/model/BrawlerData.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/model/BrawlerData.kt
@@ -4,15 +4,34 @@ data class Class(
     val id: Int,
     val name: String
 )
+open class AbilityData(
+    open val description: String,
+    open val descriptionHtml: String,
+    open val id: Long,
+    open val imageUrl: String,
+    open val name: String,
+    open val path: String,
+    open val released: Boolean,
+    open val version: Int
+)
 data class GadgetData(
-    val description: String,
-    val descriptionHtml: String,
-    val id: Int,
-    val imageUrl: String,
-    val name: String,
-    val path: String,
-    val released: Boolean,
-    val version: Int
+    override val description: String,
+    override val descriptionHtml: String,
+    override val id: Long,
+    override val imageUrl: String,
+    override val name: String,
+    override val path: String,
+    override val released: Boolean,
+    override val version: Int
+) : AbilityData(
+    description,
+    descriptionHtml,
+    id,
+    imageUrl,
+    name,
+    path,
+    released,
+    version
 )
 
 
