f23304f0496296fbbfe5812b389a4c4cb02aa7e3
itsma8di <mrmmrahmati@gmail.com>
2025-04-26 18:45:28 +0330

    feat: add icon routes to BrawlifyApi

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/BrawlifyApi.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/BrawlifyApi.kt
@@ -1,5 +1,6 @@
 package com.nothingmotion.brawlprogressionanalyzer.data.remote
 
+import android.graphics.Bitmap
 import com.nothingmotion.brawlprogressionanalyzer.data.remote.model.APIIcons
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.BrawlerData
 import retrofit2.http.GET
@@ -14,4 +15,16 @@ interface BrawlifyApi {
 
     @GET("icons")
     suspend fun getIcons(): APIIcons
+
+    @GET("gadgets/borderless/{id}")
+    suspend fun getGadget(@Path("id") id: String) : ByteArray
+
+    @GET("star-powers/borderless/{id}")
+    suspend fun getStarPower(@Path("id") id: String) : ByteArray
+
+    @GET("gears/borderless/{id}")
+    suspend fun getGear(@Path("id") id: String) : ByteArray
+
+    @GET("tiers/regular/{id}")
+    suspend fun getTier(@Path("id") id: String) : ByteArray
 }
