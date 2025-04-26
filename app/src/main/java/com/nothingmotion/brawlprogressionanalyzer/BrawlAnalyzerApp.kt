d7e38b88dbd0fcecd270942293d2b56da7852231
itsma8di <mrmmrahmati@gmail.com>
2025-04-26 18:42:49 +0330

    feat: initialized crashlytics worker on Application class

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/BrawlAnalyzerApp.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/BrawlAnalyzerApp.kt
@@ -2,31 +2,55 @@ package com.nothingmotion.brawlprogressionanalyzer
 
 import android.app.Application
 import android.graphics.Bitmap
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
 import com.github.mikephil.charting.BuildConfig
 import com.nothingmotion.brawlprogressionanalyzer.di.PreferencesManagerModule
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import androidx.collection.LruCache
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import androidx.work.ListenableWorker
+import androidx.work.WorkerFactory
+import android.content.Context
+import androidx.work.WorkerParameters
+import com.jakewharton.threetenabp.AndroidThreeTen
+import com.nothingmotion.brawlprogressionanalyzer.crashlytics.common.CrashLytics
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.BrawlerDataNinja
+import com.nothingmotion.brawlprogressionanalyzer.crashlytics.common.CrashLyticsWorkerFactory
+import com.nothingmotion.brawlprogressionanalyzer.data.PreferencesManager
+import com.nothingmotion.brawlprogressionanalyzer.util.GlobalExceptionHandling
+import javax.inject.Inject
 
 /**
  * Application class for Brawl Progression Analyzer
  * Initializes dependencies and global components
  */
 @HiltAndroidApp
-class BrawlAnalyzerApp : Application() {
+class BrawlAnalyzerApp : Application(), Configuration.Provider {
     
     // Cache for player icon URLs (Long = icon ID, String? = image URL or null if not available)
     val iconCache = LruCache<Long, String>(100) // Cache up to 100 icons
-
     val brawlerDataCache = LruCache<Long, String>(200)
+    val brawlerDataNinjaCache = LruCache<String,BrawlerDataNinja>(100)
+
+    @Inject lateinit var hiltWorkerFactory: HiltWorkerFactory
+    @Inject lateinit var crashLyticsWorkerFactory: CrashLyticsWorkerFactory
+
+    @Inject lateinit var preferencesManager: PreferencesManager
     override fun onCreate() {
         super.onCreate()
 
+        GlobalExceptionHandling.setup(this.applicationContext)
+        CrashLytics.ExceptionHandler.setup(this.applicationContext,preferencesManager)
+        AndroidThreeTen.init(this)
+
         // Initialize Timber for logging
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
+//        if (BuildConfig.DEBUG) {
+//            Log.d("BrawlerAnalyzerApp","Setting up Timber library")
+//            Timber.plant(Timber.DebugTree())
+//        }
 
         val prefsManager  = PreferencesManagerModule.getPreferencesManager(this)
         // Apply the saved theme before Hilt is initialized (using static accessor)
@@ -39,4 +63,15 @@ class BrawlAnalyzerApp : Application() {
             else AppCompatDelegate.MODE_NIGHT_NO
         )
     }
-} 
+
+    override fun getWorkManagerConfiguration(): Configuration {
+
+        Timber.tag("BrawlAnalyzerApp").d("getWorkManagerConfiguration() called")
+
+        
+        return Configuration.Builder()
+            .setWorkerFactory(hiltWorkerFactory)
+            .setMinimumLoggingLevel(Log.VERBOSE)
+            .build()
+    }
+}
