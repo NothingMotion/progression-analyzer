9ab595f8cb2cf2f46c336330829f33602766c72b
itsma8di <mrmmrahmati@gmail.com>
2025-04-27 23:23:58 +0330

    refactor: enhance error handling in NotMotRepositoryImpl

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/repository/NotMotRepositoryImpl.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/repository/NotMotRepositoryImpl.kt
@@ -5,12 +5,10 @@ import com.nothingmotion.brawlprogressionanalyzer.crashlytics.common.CrashLytics
 import com.nothingmotion.brawlprogressionanalyzer.data.remote.ProgressionAnalyzerAPI
 import com.nothingmotion.brawlprogressionanalyzer.data.remote.model.APIUpdate
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.DataError
+import com.nothingmotion.brawlprogressionanalyzer.util.DataErrorUtils
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Result
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Track
 import com.nothingmotion.brawlprogressionanalyzer.domain.repository.NotMotRepository
-import retrofit2.HttpException
-import timber.log.Timber
-import java.io.IOException
 import javax.inject.Inject
 
 class NotMotRepositoryImpl @Inject constructor() : NotMotRepository {
@@ -19,22 +17,8 @@ class NotMotRepositoryImpl @Inject constructor() : NotMotRepository {
     override suspend fun getLatestUpdate(token: String): Result<APIUpdate, DataError.NetworkError> {
         try {
             return Result.Success(api.getLatestUpdate("Bearer $token"))
-        }
-        catch(e: IOException){
-            return Result.Error(DataError.NetworkError.NO_INTERNET_CONNECTION)
-        }
-        catch(e: HttpException){
-            return when (e.code()){
-                400 -> Result.Error(DataError.NetworkError.NETWORK_ERROR)
-                401 -> Result.Error(DataError.NetworkError.UNAUTHORIZED)
-                403 -> Result.Error(DataError.NetworkError.FORBIDDEN)
-                429 -> Result.Error(DataError.NetworkError.TOO_MANY_REQUESTS)
-                500 -> Result.Error(DataError.NetworkError.SERVER_ERROR)
-                else -> Result.Error(DataError.NetworkError.UNKNOWN)
-            }
-        }
-        catch(e :Exception){
-            return Result.Error(DataError.NetworkError.UNKNOWN)
+        } catch(e :Exception){
+            return Result.Error(DataErrorUtils.handleHttpException(e))
         }
     }
 
@@ -44,22 +28,8 @@ class NotMotRepositoryImpl @Inject constructor() : NotMotRepository {
     ): Result<Track, DataError.NetworkError> {
         try {
             return Result.Success(api.newTrack("Bearer $token",data))
-        }
-        catch(e: IOException){
-            return Result.Error(DataError.NetworkError.NO_INTERNET_CONNECTION)
-        }
-        catch(e: HttpException){
-            return when (e.code()){
-                400 -> Result.Error(DataError.NetworkError.NETWORK_ERROR)
-                401 -> Result.Error(DataError.NetworkError.UNAUTHORIZED)
-                403 -> Result.Error(DataError.NetworkError.FORBIDDEN)
-                429 -> Result.Error(DataError.NetworkError.TOO_MANY_REQUESTS)
-                500 -> Result.Error(DataError.NetworkError.SERVER_ERROR)
-                else -> Result.Error(DataError.NetworkError.UNKNOWN)
-            }
-        }
-        catch(e: Exception){
-            return Result.Error(DataError.NetworkError.UNKNOWN)
+        } catch(e: Exception){
+            return Result.Error(DataErrorUtils.handleHttpException(e))
         }
     }
 
@@ -69,27 +39,10 @@ class NotMotRepositoryImpl @Inject constructor() : NotMotRepository {
     ): Result<CrashLytics.CrashReport, DataError.NetworkError> {
         return try{
             Result.Success(api.newCrash("Bearer $token",data))
-        }
-        catch(e: IOException){
-            return Result.Error(DataError.NetworkError.NO_INTERNET_CONNECTION)
-        }
-        catch(e: HttpException){
-            return when (e.code()){
-                400 -> Result.Error(DataError.NetworkError.NETWORK_ERROR)
-                401 -> Result.Error(DataError.NetworkError.UNAUTHORIZED)
-                403 -> Result.Error(DataError.NetworkError.FORBIDDEN)
-                429 -> Result.Error(DataError.NetworkError.TOO_MANY_REQUESTS)
-                500 -> Result.Error(DataError.NetworkError.SERVER_ERROR)
-                else -> {
-                    Timber.tag("NotMotRepositoryImpl").e(e);
-                    Result.Error(DataError.NetworkError.UNKNOWN)
-                }
-            }
-        }
-        catch (e:Exception){
+        }catch (e:Exception){
 
             Log.e("NotMotRepositoryImpl",e.stackTraceToString())
-            Result.Error(DataError.NetworkError.UNKNOWN)
+            Result.Error(DataErrorUtils.handleHttpException(e))
         }
     }
 }
