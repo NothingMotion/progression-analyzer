b003daef3ef07adfb914a17e4f265c0f2b9683bd
itsma8di <mrmmrahmati@gmail.com>
2025-04-27 23:17:39 +0330

    feat: update ApplicationDatabase to include new entities and DAOs for caching and progress management

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/db/ApplicationDatabase.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/db/ApplicationDatabase.kt
@@ -4,25 +4,35 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.nothingmotion.brawlprogressionanalyzer.data.db.dao.AccountDao
-import com.nothingmotion.brawlprogressionanalyzer.data.db.dao.BrawlerDao
+import com.nothingmotion.brawlprogressionanalyzer.data.db.dao.CacheDao
 import com.nothingmotion.brawlprogressionanalyzer.data.db.dao.PlayerDao
+import com.nothingmotion.brawlprogressionanalyzer.data.db.dao.ProgressDao
 import com.nothingmotion.brawlprogressionanalyzer.data.db.models.AccountEntity
-import com.nothingmotion.brawlprogressionanalyzer.data.db.models.BrawlerEntity
+import com.nothingmotion.brawlprogressionanalyzer.data.db.models.CacheEntity
 import com.nothingmotion.brawlprogressionanalyzer.data.db.models.PlayerEntity
+import com.nothingmotion.brawlprogressionanalyzer.data.db.models.PlayerHistoryEntity
+import com.nothingmotion.brawlprogressionanalyzer.data.db.models.ProgressEntity
 
 @Database(
-    entities = [AccountEntity::class, PlayerEntity::class,BrawlerEntity::class],
+    entities = [
+        AccountEntity::class, 
+        PlayerEntity::class,
+        ProgressEntity::class,
+        PlayerHistoryEntity::class,
+        CacheEntity::class
+    ],
     version = 1,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
-abstract class ApplicationDatabase : RoomDatabase(){
+abstract class ApplicationDatabase : RoomDatabase() {
 
     abstract fun playerDao(): PlayerDao
-
     abstract fun accountDao(): AccountDao
+    abstract fun progressDao(): ProgressDao
+    abstract fun cacheDao(): CacheDao
 
-    abstract fun brawlerDao(): BrawlerDao
+
     companion object {
         const val DATABASE_NAME = "brawl_progression_analyzer.db"
     }
