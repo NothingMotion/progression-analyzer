27234cd33c4621d0f324b732d760ef205ca87443
itsma8di <mrmmrahmati@gmail.com>
2025-04-27 23:22:30 +0330

    refactor: streamline error handling in BrawlerTableRepositoryImpl by utilizing DataErrorUtils

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/repository/BrawlerTableRepositoryImpl.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/repository/BrawlerTableRepositoryImpl.kt
@@ -3,12 +3,11 @@ package com.nothingmotion.brawlprogressionanalyzer.data.remote.repository
 import com.nothingmotion.brawlprogressionanalyzer.data.remote.ProgressionAnalyzerAPI
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.BrawlerTable
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.DataError
+import com.nothingmotion.brawlprogressionanalyzer.util.DataErrorUtils
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Result
 import com.nothingmotion.brawlprogressionanalyzer.domain.repository.BrawlerTableRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import retrofit2.HttpException
-import java.io.IOException
 import javax.inject.Inject
 
 class BrawlerTableRepositoryImpl @Inject constructor(): BrawlerTableRepository {
@@ -18,22 +17,9 @@ class BrawlerTableRepositoryImpl @Inject constructor(): BrawlerTableRepository {
             try {
                 emit(Result.Success(api.getBrawlerTable("Bearer $token")))
             }
-            catch (e: IOException) {
-                emit(Result.Error(DataError.NetworkError.NO_INTERNET_CONNECTION))
-            } catch (e: HttpException) {
-                emit(
-                    when (e.code()) {
-                        400 -> Result.Error(DataError.NetworkError.NETWORK_ERROR)
-                        401 -> Result.Error(DataError.NetworkError.UNAUTHORIZED)
-                        403 -> Result.Error(DataError.NetworkError.FORBIDDEN)
-                        429 -> Result.Error(DataError.NetworkError.TOO_MANY_REQUESTS)
-                        500 -> Result.Error(DataError.NetworkError.SERVER_ERROR)
-                        else -> Result.Error(DataError.NetworkError.UNKNOWN)
-                    }
-                )
-            }
+
             catch (e: Exception) {
-                emit(Result.Error(DataError.NetworkError.UNKNOWN))
+                emit(Result.Error(DataErrorUtils.handleHttpException(e)))
             }
         }
 
