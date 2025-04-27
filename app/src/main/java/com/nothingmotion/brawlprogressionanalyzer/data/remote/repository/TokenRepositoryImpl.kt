1a9e6c46cfc951d10a3cb8ce5d53f839143fe111
itsma8di <mrmmrahmati@gmail.com>
2025-04-27 23:23:03 +0330

    refactor: enhance error handling in TokenRepositoryImpl by utilizing DataErrorUtils

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/repository/TokenRepositoryImpl.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/repository/TokenRepositoryImpl.kt
@@ -1,16 +1,11 @@
 package com.nothingmotion.brawlprogressionanalyzer.data.remote.repository
 
-import android.util.Log
 import com.nothingmotion.brawlprogressionanalyzer.data.remote.ProgressionAnalyzerAPI
 import com.nothingmotion.brawlprogressionanalyzer.data.remote.model.APIToken
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.DataError
+import com.nothingmotion.brawlprogressionanalyzer.util.DataErrorUtils
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Result
 import com.nothingmotion.brawlprogressionanalyzer.domain.repository.TokenRepository
-import retrofit2.HttpException
-import timber.log.Timber
-import java.io.IOException
-import java.io.InterruptedIOException
-import java.util.logging.Logger
 import javax.inject.Inject
 
 
@@ -20,52 +15,16 @@ class TokenRepositoryImpl @Inject constructor():  TokenRepository{
         try{
             return Result.Success(api.getAccessToken("Bearer $frontEndToken"))
         }
-        catch(e: InterruptedIOException){
-            return Result.Error(DataError.NetworkError.TIMEOUT)
-        }
-        catch(e: IOException){
-
-            Timber.tag("TokenRepositoryImpl").e(e)
-            return Result.Error(DataError.NetworkError.NO_INTERNET_CONNECTION)
-        }
-        catch(e: HttpException){
-            return when (e.code()){
-                400 -> Result.Error(DataError.NetworkError.NETWORK_ERROR)
-                401 -> Result.Error(DataError.NetworkError.UNAUTHORIZED)
-                403 -> Result.Error(DataError.NetworkError.FORBIDDEN)
-                429 -> Result.Error(DataError.NetworkError.TOO_MANY_REQUESTS)
-                500 -> Result.Error(DataError.NetworkError.SERVER_ERROR)
-                else -> Result.Error(DataError.NetworkError.UNKNOWN)
-            }
-        }
         catch(e: Exception){
-            return Result.Error(DataError.NetworkError.UNKNOWN)
+            return Result.Error(DataErrorUtils.handleHttpException(e))
         }
     }
 
     override suspend fun validateAccessToken(accessToken: String): Result<String, DataError.NetworkError> {
         try{
             return Result.Success(api.validateAccessToken("Bearer $accessToken").toString())
-        }
-
-
-        catch(e: IOException){
-            Timber.tag("TokenRepositoryImpl").e(e)
-            return Result.Error(DataError.NetworkError.NO_INTERNET_CONNECTION)
-        }
-        catch(e: HttpException){
-            Timber.tag("TokenRepositoryImpl").e(e.message())
-            return when (e.code()){
-                400 -> Result.Error(DataError.NetworkError.NETWORK_ERROR)
-                401 -> Result.Error(DataError.NetworkError.UNAUTHORIZED)
-                403 -> Result.Error(DataError.NetworkError.FORBIDDEN)
-                429 -> Result.Error(DataError.NetworkError.TOO_MANY_REQUESTS)
-                500 -> Result.Error(DataError.NetworkError.SERVER_ERROR)
-                else -> Result.Error(DataError.NetworkError.UNKNOWN)
-            }
-        }
-        catch(e: Exception){
-            return Result.Error(DataError.NetworkError.UNKNOWN)
+        }catch(e: Exception){
+            return Result.Error(DataErrorUtils.handleHttpException(e))
         }
     }
 }
