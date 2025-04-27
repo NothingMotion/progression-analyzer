81bc35718a4909aeb9e7fc331cca8f74b458c181
itsma8di <mrmmrahmati@gmail.com>
2025-04-27 23:18:08 +0330

    feat: add TypeConverters for Brawler, ProgressType, and AbilityDataNinja lists in Converters class

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/db/Converters.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/db/Converters.kt
@@ -2,13 +2,26 @@ package com.nothingmotion.brawlprogressionanalyzer.data.db
 
 import androidx.room.TypeConverter
 import com.google.gson.GsonBuilder
+import com.nothingmotion.brawlprogressionanalyzer.data.db.models.ProgressType
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.AbilityDataNinja
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.Brawler
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Gadget
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.GadgetDataNinja
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Gear
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.StarPower
 import java.text.SimpleDateFormat
 import java.util.Date
 
 class Converters {
+    @TypeConverter
+    fun fromBrawlersList(value: List<Brawler>): String?{
+        return GsonBuilder().create().toJson(value)
+    }
+
+    @TypeConverter
+    fun toBrawlersList(value: String): List<Brawler>?{
+        return GsonBuilder().create().fromJson(value,Array<Brawler>::class.java)?.toList()
+    }
     @TypeConverter
     fun fromGadgetsList(value: List<Gadget>?): String? {
 
@@ -51,5 +64,24 @@ class Converters {
     fun toDate(value: String): Date? {
         val sdf = SimpleDateFormat.getDateInstance()
         return sdf.parse(value)
+    }
+    
+    @TypeConverter
+    fun fromProgressType(value: ProgressType): String {
+        return value.name
+    }
+    
+    @TypeConverter
+    fun toProgressType(value: String): ProgressType {
+        return ProgressType.valueOf(value)
+    }
+
+    @TypeConverter
+    fun fromGadgetDataNinjaList(value: List<AbilityDataNinja>):String {
+        return GsonBuilder().create().toJson(value)
+    }
+    @TypeConverter
+    fun toGadgetDataNinjaList(value: String): List<AbilityDataNinja> {
+        return GsonBuilder().create().fromJson(value, Array<AbilityDataNinja>::class.java).toList()
     }
 }
