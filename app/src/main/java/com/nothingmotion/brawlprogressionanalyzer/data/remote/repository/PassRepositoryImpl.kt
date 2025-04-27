5f8a67542eab40d783c7fef1f37572463654d535
itsma8di <mrmmrahmati@gmail.com>
2025-04-27 23:23:27 +0330

    refactor: enhance error handling in PassRepositoryImpl by utilizing DataErrorUtils

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/repository/PassRepositoryImpl.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/repository/PassRepositoryImpl.kt
@@ -2,11 +2,10 @@ package com.nothingmotion.brawlprogressionanalyzer.data.remote.repository
 
 import com.nothingmotion.brawlprogressionanalyzer.data.remote.ProgressionAnalyzerAPI
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.DataError
+import com.nothingmotion.brawlprogressionanalyzer.util.DataErrorUtils
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.PassRewards
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.Result
 import com.nothingmotion.brawlprogressionanalyzer.domain.repository.PassRepository
-import retrofit2.HttpException
-import java.io.IOException
 import javax.inject.Inject
 
 class PassRepositoryImpl @Inject constructor(): PassRepository {
@@ -14,72 +13,24 @@ class PassRepositoryImpl @Inject constructor(): PassRepository {
     override suspend fun getPassFreeTable(token: String): Result<PassRewards, DataError.NetworkError> {
         try {
             return Result.Success(api.getPassFreeRewards("Bearer $token"))
-        }
-
-
-        catch(e: IOException){
-            return Result.Error(DataError.NetworkError.NO_INTERNET_CONNECTION)
-        }
-        catch(e: HttpException){
-            return when (e.code()){
-                400 -> Result.Error(DataError.NetworkError.NETWORK_ERROR)
-                401 -> Result.Error(DataError.NetworkError.UNAUTHORIZED)
-                403 -> Result.Error(DataError.NetworkError.FORBIDDEN)
-                429 -> Result.Error(DataError.NetworkError.TOO_MANY_REQUESTS)
-                500 -> Result.Error(DataError.NetworkError.SERVER_ERROR)
-                else -> Result.Error(DataError.NetworkError.UNKNOWN)
-            }
-        }
-        catch(e :Exception){
-            return Result.Error(DataError.NetworkError.UNKNOWN)
+        }catch(e :Exception){
+            return Result.Error(DataErrorUtils.handleHttpException(e))
         }
     }
 
     override suspend fun getPassPremiumTable(token: String): Result<PassRewards, DataError.NetworkError> {
         try {
             return Result.Success(api.getPassPremiumRewards("Bearer $token"))
-        }
-
-
-        catch(e: IOException){
-            return Result.Error(DataError.NetworkError.NO_INTERNET_CONNECTION)
-        }
-        catch(e: HttpException){
-            return when (e.code()){
-                400 -> Result.Error(DataError.NetworkError.NETWORK_ERROR)
-                401 -> Result.Error(DataError.NetworkError.UNAUTHORIZED)
-                403 -> Result.Error(DataError.NetworkError.FORBIDDEN)
-                429 -> Result.Error(DataError.NetworkError.TOO_MANY_REQUESTS)
-                500 -> Result.Error(DataError.NetworkError.SERVER_ERROR)
-                else -> Result.Error(DataError.NetworkError.UNKNOWN)
-            }
-        }
-        catch(e :Exception){
-            return Result.Error(DataError.NetworkError.UNKNOWN)
+        }catch(e :Exception){
+            return Result.Error(DataErrorUtils.handleHttpException(e))
         }
     }
 
     override suspend fun getPassPlusTable(token: String): Result<PassRewards, DataError.NetworkError> {
         try {
             return Result.Success(api.getPassPlusRewards("Bearer $token"))
-        }
-
-
-        catch(e: IOException){
-            return Result.Error(DataError.NetworkError.NO_INTERNET_CONNECTION)
-        }
-        catch(e: HttpException){
-            return when (e.code()){
-                400 -> Result.Error(DataError.NetworkError.NETWORK_ERROR)
-                401 -> Result.Error(DataError.NetworkError.UNAUTHORIZED)
-                403 -> Result.Error(DataError.NetworkError.FORBIDDEN)
-                429 -> Result.Error(DataError.NetworkError.TOO_MANY_REQUESTS)
-                500 -> Result.Error(DataError.NetworkError.SERVER_ERROR)
-                else -> Result.Error(DataError.NetworkError.UNKNOWN)
-            }
-        }
-        catch(e :Exception){
-            return Result.Error(DataError.NetworkError.UNKNOWN)
+        }catch(e :Exception){
+            return Result.Error(DataErrorUtils.handleHttpException(e))
         }
     }
 }
