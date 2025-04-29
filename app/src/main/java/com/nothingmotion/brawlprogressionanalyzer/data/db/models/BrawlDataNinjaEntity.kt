e23fc1f008d9163874e3fb3311a1b59512754df6
itsma8di <mrmmrahmati@gmail.com>
2025-04-29 20:52:46 +0330

    feat: update BrawlDataNinjaEntity with new properties and conversion methods

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/db/models/BrawlDataNinjaEntity.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/db/models/BrawlDataNinjaEntity.kt
@@ -1,15 +1,47 @@
 package com.nothingmotion.brawlprogressionanalyzer.data.db.models
 
 import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.BrawlerDataNinja
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.GadgetDataNinja
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.StarPowerDataNinja
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.Status
+import java.util.Date
 
-@Entity(tableName = "brawl_data_ninja")
+@Entity(tableName = "brawl_data_ninja", indices = [Index(value = ["id"], unique = true), Index(value = ["name"], unique = true)])
 data class BrawlDataNinjaEntity(
-    val id: String,
+    @PrimaryKey(autoGenerate = true) val id: Long,
     val name: String,
     val description: String,
     val rarity: String,
     val gadgets: List<GadgetDataNinja>,
-    val starpowers: List<StarPowerDataNinja>
+    val starpowers: List<StarPowerDataNinja>,
+
+    val createdAt: Date
 )
+
+
+fun BrawlDataNinjaEntity.toDomain(): com.nothingmotion.brawlprogressionanalyzer.domain.model.BrawlerDataNinja {
+    return com.nothingmotion.brawlprogressionanalyzer.domain.model.BrawlerDataNinja(
+        id = id.toString(),
+        name = name,
+        description = description,
+        status = Status(rarity),
+        gadgets = gadgets,
+        starpowers = starpowers,
+    )
+}
+
+
+fun BrawlerDataNinja.toDatabase(): BrawlDataNinjaEntity {
+    return BrawlDataNinjaEntity(
+        id = 0,
+        name = name,
+        description = description,
+        rarity = "",
+        gadgets = gadgets,
+        starpowers = starpowers,
+        createdAt= Date()
+    )
+}
