0ad38ac780c0eabb1203e27a79e744dd7535bfee
itsma8di <mrmmrahmati@gmail.com>
2025-04-29 20:52:33 +0330

    feat: add methods for inserting and querying BrawlDataNinja entities

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/db/dao/BrawlDataNinjaDao.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/db/dao/BrawlDataNinjaDao.kt
@@ -2,10 +2,23 @@ package com.nothingmotion.brawlprogressionanalyzer.data.db.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.nothingmotion.brawlprogressionanalyzer.data.db.models.BrawlDataNinjaEntity
 
 @Dao
 interface BrawlDataNinjaDao {
     @Insert(onConflict = androidx.room.OnConflictStrategy.REPLACE)
     suspend fun insertBrawlDataNinja(brawlDataNinja: BrawlDataNinjaEntity)
+
+
+    @Insert(onConflict = androidx.room.OnConflictStrategy.REPLACE)
+    suspend fun insertBrawlDataNinjaList(brawlDataNinjaList: List<BrawlDataNinjaEntity>)
+
+
+    @Query("SELECT * FROM brawl_data_ninja WHERE name LIKE :name LIMIT 1")
+    suspend fun getBrawlDataNinja(name: String): BrawlDataNinjaEntity?
+
+
+    @Query("SELECT * FROM brawl_data_ninja")
+    suspend fun getAllBrawlDataNinja(): List<BrawlDataNinjaEntity>
 }
