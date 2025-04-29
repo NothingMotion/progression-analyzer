d2c45e28c7668784ee87abcd364b38bb71cb18b7
itsma8di <mrmmrahmati@gmail.com>
2025-04-29 16:41:42 +0330

    refactor: update StarPowerData to implement AbilityData interface

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/model/BrawlerData.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/domain/model/BrawlerData.kt
@@ -38,14 +38,23 @@ data class GadgetData(
 
 
 data class StarPowerData(
-    val description: String,
-    val descriptionHtml: String,
-    val id: Int,
-    val imageUrl: String,
-    val name: String,
-    val path: String,
-    val released: Boolean,
-    val version: Int
+    override val description: String,
+    override val descriptionHtml: String,
+    override val id: Long,
+    override val imageUrl: String,
+    override val name: String,
+    override val path: String,
+    override val released: Boolean,
+    override val version: Int
+) : AbilityData (
+    description,
+    descriptionHtml,
+    id,
+    imageUrl,
+    name,
+    path,
+    released,
+    version
 )
 data class Rarity(
     val color: String,
