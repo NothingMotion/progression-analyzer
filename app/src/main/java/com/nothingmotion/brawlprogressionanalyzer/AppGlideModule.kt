803d8429bee1511210625f422172f8872262d8be
itsma8di <mrmmrahmati@gmail.com>
2025-04-29 16:41:29 +0330

    refactor: rename AppGlideModule.kt and update package declaration

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/AppGlideModule.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/AppGlideModule.kt
@@ -1,4 +1,4 @@
-package com.nothingmotion.brawlprogressionanalyzer.di
+package com.nothingmotion.brawlprogressionanalyzer
 
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
