5708190df9f1a0889b84ba23d5ed8c152f3d06c4
itsma8di <mrmmrahmati@gmail.com>
2025-05-01 16:40:34 +0330

    fix: mapping api to domain model to BrawlerTableRepositoryImpl

--- app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/repository/BrawlerTableRepositoryImpl.kt
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/repository/BrawlerTableRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.nothingmotion.brawlprogressionanalyzer.data.remote.repository
 
 import com.nothingmotion.brawlprogressionanalyzer.data.remote.ProgressionAnalyzerAPI
+import com.nothingmotion.brawlprogressionanalyzer.data.remote.mappers.toBrawlerTable
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.BrawlerTable
 import com.nothingmotion.brawlprogressionanalyzer.domain.model.DataError
 import com.nothingmotion.brawlprogressionanalyzer.util.DataErrorUtils
@@ -8,6 +9,7 @@ import com.nothingmotion.brawlprogressionanalyzer.domain.model.Result
 import com.nothingmotion.brawlprogressionanalyzer.domain.repository.BrawlerTableRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import timber.log.Timber
 import javax.inject.Inject
 
 class BrawlerTableRepositoryImpl @Inject constructor(): BrawlerTableRepository {
@@ -15,10 +17,11 @@ class BrawlerTableRepositoryImpl @Inject constructor(): BrawlerTableRepository {
     override suspend fun getBrawlerTable(token: String): Flow<Result<List<BrawlerTable>,DataError.NetworkError>> {
         return flow {
             try {
-                emit(Result.Success(api.getBrawlerTable("Bearer $token")))
+                emit(Result.Success(api.getBrawlerTable("Bearer $token").table.map { it.toBrawlerTable() }))
             }
 
             catch (e: Exception) {
+                Timber.tag("BrawlerTableRepository").e(e, "Error fetching brawler table: $e")
                 emit(Result.Error(DataErrorUtils.handleHttpException(e)))
             }
         }
