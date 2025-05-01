9c7439c3d0e30da7e2b0ce9aaac92dcfa0ac1305
itsma8di <mrmmrahmati@gmail.com>
2025-05-01 16:35:50 +0330

    feat: add APIUpgradeTable for api response

---
+++ app/src/main/java/com/nothingmotion/brawlprogressionanalyzer/data/remote/model/APIUpgradeTable.kt
@@ -0,0 +1,7 @@
+package com.nothingmotion.brawlprogressionanalyzer.data.remote.model
+
+import com.nothingmotion.brawlprogressionanalyzer.domain.model.UpgradeTable
+
+data class APIUpgradeTable(
+    val table: UpgradeTable
+)
